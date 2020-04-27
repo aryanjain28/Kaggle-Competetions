b6a5c004fc47afb8cedcc05ba08f2a3a215a8b78
Aryan Jain <aryanjn807@gmail.com>
2020-04-27 19:24:18 +0530

    ImageGen

--- main.py
+++ main.py
@@ -1,13 +1,49 @@
+import warnings
+warnings.filterwarnings('ignore')
+
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, Flatten, MaxPooling2D, Dropout, Dense
 from tensorflow import keras
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import pygame
 
 import os
 import random
 import numpy as np
 import pandas as pd
-import matplotlib.ptplot as plt
+import matplotlib.pyplot as plt
+
+BATCH_SIZE = 100
+EPOCHS = 10
+WIDTH = 256
+HEIGHT = 256
+
+trainDir = os.path.join(os.path.dirname(__file__), 'images/train')
+testDir = os.path.join(os.path.dirname(__file__), 'images/test')
+
+cats = os.path.join(os.path.dirname(__file__), 'images/train/cats')
+dogs = os.path.join(os.path.dirname(__file__), 'images/train/dogs')
+
+imgGen = ImageDataGenerator(rescale=1./255)
+
+trainData = imgGen.flow_from_directory(
+    directory=trainDir,
+    target_size=(WIDTH, HEIGHT),
+    color_mode='rgb',
+    class_mode='binary',
+    batch_size=BATCH_SIZE,
+    shuffle=True
+)
+
+# testData = imgGen.flow_from_directory(
+#     directory=testDir,
+#     target_size=(WIDTH, HEIGHT),
+#     color_mode='rgb',
+#     class_mode='binary',
+#     batch_size=BATCH_SIZE
+# )
+
+
 
 
